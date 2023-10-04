95866319a4aff6dc98f15a0f1c4e54b92125485b
Henri Simmons <henri.simmons.hayden@gmail.com>
2023-10-04 14:40:35 +0100

    added new task

--- wk3/lab3-findMax.c
+++ wk3/lab3-findMax.c
@@ -1,10 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int findMax(int)
+// Function to find the maximum element in an array
+int findMax(int *arr, int size)
 {
+    int max = arr[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
 }
 
 int main(int argc, char *argv[])
 {
+
+    // Calculate the number of integer arguments
+    int numArgs = argc - 1;
+
+    // Allocate memory for an integer array to store the input values
+    int *arr = (int *)malloc(numArgs * sizeof(int));
+
+    // Convert command-line arguments to integers and store them in the array
+    for (int i = 1; i <= numArgs; i++)
+    {
+        arr[i - 1] = atoi(argv[i]);
+    }
+
+    // Find the maximum element in the array
+    int max = findMax(arr, numArgs);
+
+    // Print the maximum element
+    printf("%d\n", max);
+
+    // Free dynamically allocated memory
+    free(arr);
+
+    return 0;
 }
