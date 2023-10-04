522b2ad0d0a948a3837bba31595afe800e60d05d
Henri Simmons <henri.simmons.hayden@gmail.com>
2023-10-04 14:58:07 +0100

    added a gitignore file

---
+++ wk3/lab3-search-number.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    // find first number to search through array with and length of array
+    int searchNum = atoi(argv[1]);
+    int length = argc - 2;
+}
