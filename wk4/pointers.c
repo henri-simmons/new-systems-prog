275e093eba94cbbaa4ecc29da59cefe1a9cb896f
henri-simmons <henri.simmons.hayden@gmail.com>
2023-11-02 11:09:23 +0000

    added folder for pointers

---
+++ wk4/pointers.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int number = 15;        /* we declare a varaible stored an integer value 15 */
+    int *pointer = &number; /*we use a `pointer` to point to the address of `number` */
+}
